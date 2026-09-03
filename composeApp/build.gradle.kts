95ba08db7dd3d9755d7597f3bfb56e8946155cb7
sampaiofa-tech <sampaiofa@gmail.com>
2026-09-03 09:30:40 -0300

    fix(build): MissingGoogleServicesStrategy.ERROR para falha estrita caso google-services.json falte

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -227,7 +227,7 @@ secrets {
     ignoreList.add("FIREBASE_APPCHECK_DEBUG_TOKEN")
 }
 
-googleServices { missingGoogleServicesStrategy = MissingGoogleServicesStrategy.FAIL }
+googleServices { missingGoogleServicesStrategy = MissingGoogleServicesStrategy.ERROR }
 
 dependencies {
     "kspAndroid"(libs.androidx.room.compiler)
