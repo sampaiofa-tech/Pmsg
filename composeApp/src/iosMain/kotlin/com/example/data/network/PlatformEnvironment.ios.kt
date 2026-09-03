a9fe76c8ff098d77a8846b9b4f08361abe12b523
sampaiofa-tech <sampaiofa@gmail.com>
2026-09-03 10:56:26 -0300

    fix(ios): importa extensao timeIntervalSince1970 explicitamente em PlatformEnvironment.ios.kt

--- composeApp/src/iosMain/kotlin/com/example/data/network/PlatformEnvironment.ios.kt
+++ composeApp/src/iosMain/kotlin/com/example/data/network/PlatformEnvironment.ios.kt
@@ -1,7 +1,10 @@
 package com.example.data.network
 
+import platform.Foundation.NSDate
+import platform.Foundation.timeIntervalSince1970
+
 actual object PlatformEnvironment {
     actual val isDebug: Boolean = false
     actual fun getEnv(name: String): String? = null
-    actual fun currentTimeMillis(): Long = (platform.Foundation.NSDate().timeIntervalSince1970 * 1000.0).toLong()
+    actual fun currentTimeMillis(): Long = (NSDate().timeIntervalSince1970 * 1000.0).toLong()
 }
